d3571f2ded0830f325fb246d396f11471b600491
Onwudeuju <sophyonwude291@gmail.com>
2025-07-29 01:53:04 -0700

    second

--- src/pages/AllGoals.jsx
+++ src/pages/AllGoals.jsx
@@ -1,9 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import pen from "../assets/pen.png";
 import can from "../assets/can.png";
 
 const AllGoals = () => {
+  const [goals, setGoals] = useState([]);
+  useEffect(() => {
+    const fetchGoals = async () => {
+      try {
+        const getGoalApi = await fetch(
+          "https://goal-backend-v8uh.onrender.com/api/goals/all"
+        );
+        const goalB = await getGoalApi.json();
+        console.log(goalB);
+        setGoals(goalB);
+      } catch (error) {
+        console.log("I made a mistake");
+      }
+    };
+    fetchGoals();
+  }, []);
+
   return (
     <div className="mx-[100px] my-[32px]">
       <div className="flex items-center justify-between">
@@ -20,105 +37,68 @@ const AllGoals = () => {
 
       <div className="mt-[40px] flex flex-col gap-[60px]">
         {/* Completed Goal */}
-        <div className="text-start px-[35px] pt-[24px] pb-[50px] shadow-[0_4px_4px_rgba(0,0,0,0.2)] flex flex-col gap-[35px]">
-          <div className="flex flex-col gap-[12px]">
-            <h4 className="font-montserrat font-semibold text-[20px] text-[#0585cd] m-0">
-              Congratulations 🎉
-            </h4>
-            <h3 className="font-montserrat font-semibold text-[28px] text-black m-0">
-              Online Design Class
-            </h3>
-            <p className="font-montserrat font-normal text-[20px] leading-[24.38px] text-black/80 m-0">
-              Lorem ipsum dolor sit amet consectetur. Congue augue volutpat
-              massa turpis alin tristique. Interdum consequat consectetur eu
-              viverra consequat viverra nulla. Aliqu neque rhoncus ut
-              scelerisque eu vel ullamcorper mauris neque.
-            </p>
-          </div>
-
-          <div className="flex items-center justify-between">
-            <div className="w-[368px] flex flex-col items-start gap-[12px]">
-              <div className="flex justify-between w-full items-start">
-                <p className="font-montserrat font-normal text-[16px] text-black/80 m-0">
-                  Progress
-                </p>
-                <p className="font-montserrat font-normal text-[16px] text-black/80 m-0">
-                  100%
+        {goals.map((goal) => {
+          return (
+            <div className="text-start px-[35px] pt-[24px] pb-[50px] shadow-[0_4px_4px_rgba(0,0,0,0.2)] flex flex-col gap-[35px]">
+              <div className="flex flex-col gap-[12px]">
+                {goal.progress === 100 && (
+                  <h4 className="font-montserrat font-semibold text-[20px] text-[#0585cd] m-0">
+                    Congratulations 🎉
+                  </h4>
+                )}
+                <h3 className="font-montserrat font-semibold text-[28px] text-black m-0">
+                  {goal.title}
+                </h3>
+                <p className="font-montserrat font-normal text-[20px] leading-[24.38px] text-black/80 m-0">
+                  {goal.description}
                 </p>
               </div>
-              <div className="w-full bg-[#d9d9d9] h-[12px] rounded-[10px]">
-                <div className="h-[12px] w-full rounded-[10px] bg-[#339933]"></div>
+
+              <div className="flex items-center justify-between">
+                <div className="w-[368px] flex flex-col items-start gap-[12px]">
+                  <div className="flex justify-between w-full items-start">
+                    <p className="font-montserrat font-normal text-[16px] text-black/80 m-0">
+                      Progress
+                    </p>
+                    <p className="font-montserrat font-normal text-[16px] text-black/80 m-0">
+                      {goal.progress}%
+                    </p>
+                  </div>
+                  <div className="w-full bg-[#d9d9d9] h-[12px] rounded-[10px]">
+                    <div
+                      className="h-[12px]  rounded-[10px]"
+                      style={{
+                        width: `${goal.progress}%`,
+                        backgroundColor:
+                          goal.progress < 50 ? "#aff0000cc" : "#339933",
+                      }}
+                    ></div>
+                  </div>
+                </div>
+
+                <div className="w-[30%] flex justify-between cursor-pointer">
+                  <Link
+                    to="/progress"
+                    className="no-underline flex items-center justify-center gap-[10px] rounded-[10px] p-[16px] bg-[#0585cd] cursor-pointer"
+                  >
+                    <img src={pen} alt="Edit icon" />
+                    <p className="font-montserrat font-semibold text-[20px] text-white m-0">
+                      Edit
+                    </p>
+                  </Link>
+                  <button className="no-underline flex items-center justify-center gap-[10px] rounded-[10px] p-[16px] bg-white border border-[#0585cd] cursor-pointer">
+                    <img src={can} alt="Delete icon" />
+                    <p className="font-montserrat font-semibold text-[20px] text-[#0585cd] m-0">
+                      Delete
+                    </p>
+                  </button>
+                </div>
               </div>
             </div>
-
-            <div className="w-[30%] flex justify-between cursor-pointer">
-              <Link
-                to="/progress"
-                className="no-underline flex items-center justify-center gap-[10px] rounded-[10px] p-[16px] bg-[#0585cd] cursor-pointer"
-              >
-                <img src={pen} alt="Edit icon" />
-                <p className="font-montserrat font-semibold text-[20px] text-white m-0">
-                  Edit
-                </p>
-              </Link>
-              <button className="no-underline flex items-center justify-center gap-[10px] rounded-[10px] p-[16px] bg-white border border-[#0585cd] cursor-pointer">
-                <img src={can} alt="Delete icon" />
-                <p className="font-montserrat font-semibold text-[20px] text-[#0585cd] m-0">
-                  Delete
-                </p>
-              </button>
-            </div>
-          </div>
-        </div>
+          );
+        })}
 
         {/* Ongoing Goal */}
-        <div className="text-start px-[35px] pt-[24px] pb-[50px] shadow-[0_4px_4px_rgba(0,0,0,0.2)] flex flex-col gap-[35px]">
-          <div className="flex flex-col gap-[12px]">
-            <h3 className="font-montserrat font-semibold text-[28px] text-black m-0">
-              Landing Page Design
-            </h3>
-            <p className="font-montserrat font-normal text-[20px] leading-[24.38px] text-black/80 m-0">
-              Lorem ipsum dolor sit amet consectetur. Congue augue volutpat
-              massa turpis alin tristique. Interdum consequat consectetur eu
-              viverra consequat viverra nulla. Aliqu neque rhoncus ut
-              scelerisque eu vel ullamcorper mauris neque.
-            </p>
-          </div>
-
-          <div className="flex items-center justify-between">
-            <div className="w-[368px] flex flex-col items-start gap-[12px]">
-              <div className="flex justify-between w-full items-start">
-                <p className="font-montserrat font-normal text-[16px] text-black/80 m-0">
-                  Progress
-                </p>
-                <p className="font-montserrat font-normal text-[16px] text-black/80 m-0">
-                  30%
-                </p>
-              </div>
-              <div className="w-full bg-[#d9d9d9] h-[12px] rounded-[10px]">
-                <div className="h-[12px] rounded-[10px] w-[30%] bg-[#ff0000cc]"></div>
-              </div>
-            </div>
-
-            <div className="w-fit flex gap-[100px] cursor-pointer">
-              <Link
-                to="/progress"
-                className="no-underline flex items-center justify-center gap-[10px] rounded-[10px] p-[16px] bg-[#0585cd] cursor-pointer"
-              >
-                <img src={pen} alt="Update icon" />
-                <p className="font-montserrat font-semibold text-[20px] text-white m-0">
-                  Update Progress
-                </p>
-              </Link>
-              <button className="no-underline flex items-center justify-center gap-[10px] rounded-[10px] p-[16px] bg-white border border-[#0585cd] cursor-pointer">
-                <img src={can} alt="Delete icon" />
-                <p className="font-montserrat font-semibold text-[20px] text-[#0585cd] m-0">
-                  Delete
-                </p>
-              </button>
-            </div>
-          </div>
-        </div>
       </div>
     </div>
   );
